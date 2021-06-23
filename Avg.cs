246e0e4ad59e0fb2d41361dcf177b1f2f3ae8b7c
Yasaswini7798 <86249768+Yasaswini7798@users.noreply.github.com>
2021-06-23 13:33:22 +0530

    Add files via upload

---
+++ Avg.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VirtusaDay1
+{
+    class Avg
+    {
+        static void Main()
+        {
+            int m1, m2, m3, total = 0;
+            float avg;
+            Console.WriteLine("enter 3 marks");
+            m1 = Convert.ToInt32(Console.ReadLine());
+            m2 = Convert.ToInt32(Console.ReadLine());
+            m3 = Convert.ToInt32(Console.ReadLine());
+            total = m1 + m2 + m3;
+            avg = float.Parse(total.ToString()) / 3;
+            Console.WriteLine("total="+avg);
+            switch(avg>35?"pass":"fail")
+            {
+                case "pass":
+                    Console.WriteLine("passed");
+                    break;
+                case "fail":
+                    Console.WriteLine("failed");
+                    break;
+                default:
+                    Console.WriteLine("Not Valid");
+                    break;
+            }
+        }
+    }
+}
